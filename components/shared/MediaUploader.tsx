1198d0a3101e5d6d92de7fa2509822394be7a89c
MohitShishodia <mohitsisodia667@gmail.com>
2024-05-09 18:28:06 +0530

    Transformation Form

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,14 @@
+
+import { useToast } from "@/components/ui/use-toast"
+
+const MediaUploader = () => {
+    const {toast}=useToast();
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MediaUploader
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,10 +23,10 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imagegenie', bufferCommands: false 
+      dbName: 'imagegenie', bufferCommands: false
     })
 
   cached.conn = await cached.promise;
-  
+
   return cached.conn;
 }
